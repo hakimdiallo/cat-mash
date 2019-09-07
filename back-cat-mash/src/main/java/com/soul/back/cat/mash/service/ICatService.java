41a8dcb444f2c346198d4511123ad4121fde9d85
Souleymane DIALLO <souleymane.diallo@capgemini.com>
2019-09-07 18:27:43 +0200

    defining service interface

---
+++ back-cat-mash/src/main/java/com/soul/back/cat/mash/service/ICatService.java
@@ -0,0 +1,17 @@
+package com.soul.back.cat.mash.service;
+
+import java.util.List;
+import com.soul.back.cat.mash.bean.CatDto;
+
+public interface ICatService {
+  
+  List<CatDto> getCats();
+  
+  List<CatDto> getCatsOrderByScore();
+  
+  Integer getNumberOfScores();
+  
+  List<CatDto> getCatMash();
+
+  CatDto voteForCat(int id);
+}
